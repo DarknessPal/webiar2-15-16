1dbf2041e23809cbda289004f91152c095f11fd4
DarknessPal <nightknight11@gmail.com>
2022-11-21 18:27:19 +0200

    Splitted classes Dispatcher, Student, Group, StudyController. Removed final parameters.

--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -1,6 +1,5 @@
 import com.github.javafaker.Faker;
 
-import java.util.ArrayList;
 import java.util.*;
 
 public class Dispatcher {
@@ -30,12 +29,11 @@ public class Dispatcher {
             for (int studentIndex = 0; studentIndex < firstGroup.groupSize; studentIndex++) {
                 finalFirstGroup.getStudent(studentIndex).isFinished = true;
                 finalFirstGroup.getStudent(studentIndex).closeMark();
-                finalFirstGroup.getStudent(studentIndex).init();
             }
         }
         ///////////// Break Test ///////////////////
         Student temp1 = finalFirstGroup.students.get(0);
-        temp1.name = "You failed";
+        temp1.setName("You failed");
  //       temp1.marks = finalFirstGroup.students.setMark(1, 1);
         System.out.println("Closed group students' list");
         System.out.println(finalFirstGroup.students);
@@ -44,59 +42,5 @@ public class Dispatcher {
 //        finalFirstGroup.getStudent(0).setMark(0, 0);
 //        System.out.println(finalFirstGroup.getStudent(0).marks);
         ///////////// End of Break Test /////////////////
-    }
-    static class Group {
-        Random random = new Random();
-        int groupSize;
-        List<Student> students;
-        Group() {
-            this.groupSize = random.nextInt(30);
-            this.students = new ArrayList<>();
-        }
-        public Student getStudent(int studentIndex) {
-            return students.get(studentIndex);
-        }
-    }
-    static class Student {
-        private String name;
-        private final String finalName = init();;
-        boolean isFinished = false;
-        List<Integer> marks;
-        Student(Faker faker) {
-            this.name = faker.name().fullName();
-            this.marks = new ArrayList<>();
-        }
-        String init() {
-            System.out.println(this.name);
-            return this.name;
-        }
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            if (!isFinished) {
-                this.name = name;
-            } else{
-                this.name = null;
-                System.out.println("Can't set name in final group");
-            }
-        }
-        @Override
-        public String toString() {
-            return name;
-        }
-        public void setMark(int markIndex, int mark) {
-            this.marks.add(markIndex, mark);
-        }
-        public void closeMark() {
-            this.marks = Collections.unmodifiableList(marks);
-        }
-    }
-    static class StudyController {
-        public static Group endsStudyYear(Group g) {
-            final Group firstGroup = g;
-            firstGroup.students = Collections.unmodifiableList(firstGroup.students);
-            return firstGroup;
-        }
     }
 }
